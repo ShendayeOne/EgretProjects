b65539a79d95d3a1cd604b715d38a37a0d51061e
ShenYang <474827518@qq.com>
2023-09-21 17:34:21 +0800

    OK了

--- EgretHw541/src/Main.ts
+++ EgretHw541/src/Main.ts
@@ -1,36 +1,10 @@
-//////////////////////////////////////////////////////////////////////////////////////
-//
-//  Copyright (c) 2014-present, Egret Technology.
-//  All rights reserved.
-//  Redistribution and use in source and binary forms, with or without
-//  modification, are permitted provided that the following conditions are met:
-//
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of the Egret nor the
-//       names of its contributors may be used to endorse or promote products
-//       derived from this software without specific prior written permission.
-//
-//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
-//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
-//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-//////////////////////////////////////////////////////////////////////////////////////
-
 class Main extends egret.DisplayObjectContainer {
+    private initButton: egret.TextField;
+    private loginButton: egret.TextField;
+    private payButton: egret.TextField;
 
 
-
+    // 构造函数
     public constructor() {
         super();
         this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
@@ -47,10 +21,12 @@ class Main extends egret.DisplayObjectContainer {
         })
 
         egret.lifecycle.onPause = () => {
+            console.log("应用进入后台");
             egret.ticker.pause();
         }
 
         egret.lifecycle.onResume = () => {
+            console.log("应用进入前台");
             egret.ticker.resume();
         }
 
@@ -58,21 +34,15 @@ class Main extends egret.DisplayObjectContainer {
             console.log(e);
         })
 
-
-
     }
 
+    // 异步地加载资源并创建游戏场
     private async runGame() {
         await this.loadResource()
         this.createGameScene();
-        const result = await RES.getResAsync("description_json")
-        this.startAnimation(result);
-        await platform.login();
-        const userInfo = await platform.getUserInfo();
-        console.log(userInfo);
-
     }
 
+    // 异步加载游戏资源
     private async loadResource() {
         try {
             const loadingView = new LoadingUI();
@@ -86,8 +56,6 @@ class Main extends egret.DisplayObjectContainer {
         }
     }
 
-    private textfield: egret.TextField;
-
     /**
      * 创建游戏场景
      * Create a game scene
@@ -95,55 +63,63 @@ class Main extends egret.DisplayObjectContainer {
     private createGameScene() {
         let sky = this.createBitmapByName("bg_jpg");
         this.addChild(sky);
-        let stageW = this.stage.stageWidth;
-        let stageH = this.stage.stageHeight;
-        sky.width = stageW;
-        sky.height = stageH;
 
-        let topMask = new egret.Shape();
-        topMask.graphics.beginFill(0x000000, 0.5);
-        topMask.graphics.drawRect(0, 0, stageW, 172);
-        topMask.graphics.endFill();
-        topMask.y = 33;
-        this.addChild(topMask);
+        // 首先创建按钮
+        this.loginButton = new egret.TextField();
+        this.loginButton.text = "登录";
+        this.loginButton.textAlign = egret.HorizontalAlign.CENTER; // 设置文本水平居中
+        this.loginButton.verticalAlign = egret.VerticalAlign.MIDDLE; // 设置文本垂直居中
+        this.loginButton.width = 200; // 设置文本字段宽度
+        this.loginButton.height = 50; // 设置文本字段高度
+        this.loginButton.touchEnabled = true;
+        this.loginButton.backgroundColor = 0xFFFACD ; 
+        this.loginButton.textColor = 0xFFFFF0; 
+        this.loginButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onLoginButtonClick, this);
+        this.addChild(this.loginButton);
+        
+        // 计算文本字段位置，使其在舞台中居中
+        this.loginButton.x = (this.stage.stageWidth - this.loginButton.width) / 2;
+        this.loginButton.y = (this.stage.stageHeight - this.loginButton.height) / 2;
+        
+        this.initButton = new egret.TextField();
+        this.initButton.text = "初始化";
+        this.initButton.touchEnabled = true;
+        // this.initButton.backgroundColor = 0xFFFFF0 // 白色
+        // this.initButton.textColor = 0x000000; // 字体颜色也设置为白色
+        this.initButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onInitButtonClick, this);
+        // 计算初始化按钮的位置，使其在登录按钮上方 100 像素
+        this.initButton.x = (this.stage.stageWidth - this.initButton.width) / 2;
+        this.initButton.y = this.loginButton.y - 100;
+        this.addChild(this.initButton);
+        
+        this.payButton = new egret.TextField();
+        this.payButton.text = "支付";
+        this.payButton.touchEnabled = true;
+        // this.payButton.backgroundColor = 0xFFFFF0; // 白色
+        // this.payButton.textColor = 0x000000; // 字体颜色也设置为白色
+        this.payButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onPayButtonClick, this);
+        // 计算支付按钮的位置，使其在登录按钮下方 100 像素
+        this.payButton.x = (this.stage.stageWidth - this.payButton.width) / 2;
+        this.payButton.y = this.loginButton.y + 100;
+        this.addChild(this.payButton);
 
-        let icon = this.createBitmapByName("egret_icon_png");
-        this.addChild(icon);
-        icon.x = 26;
-        icon.y = 33;
+    }
 
-        let line = new egret.Shape();
-        line.graphics.lineStyle(2, 0xffffff);
-        line.graphics.moveTo(0, 0);
-        line.graphics.lineTo(0, 117);
-        line.graphics.endFill();
-        line.x = 172;
-        line.y = 61;
-        this.addChild(line);
-
-
-        let colorLabel = new egret.TextField();
-        colorLabel.textColor = 0xffffff;
-        colorLabel.width = stageW - 172;
-        colorLabel.textAlign = "center";
-        colorLabel.text = "Hello Egret";
-        colorLabel.size = 24;
-        colorLabel.x = 172;
-        colorLabel.y = 80;
-        this.addChild(colorLabel);
-
-        let textfield = new egret.TextField();
-        this.addChild(textfield);
-        textfield.alpha = 0;
-        textfield.width = stageW - 172;
-        textfield.textAlign = egret.HorizontalAlign.CENTER;
-        textfield.size = 24;
-        textfield.textColor = 0xffffff;
-        textfield.x = 172;
-        textfield.y = 135;
-        this.textfield = textfield;
-
-
+    private onInitButtonClick(e: egret.TouchEvent) {
+        console.log("初始化按钮被点击");
+        // 在这里执行初始化操作
+        
+    }
+    
+    private onLoginButtonClick(e: egret.TouchEvent) {
+        console.log("登录按钮被点击");
+        // 在这里执行登录操作
+        
+    }
+    
+    private onPayButtonClick(e: egret.TouchEvent) {
+        console.log("支付按钮被点击");
+        // 在这里执行支付操作
     }
 
     /**
@@ -155,35 +131,5 @@ class Main extends egret.DisplayObjectContainer {
         let texture: egret.Texture = RES.getRes(name);
         result.texture = texture;
         return result;
-    }
-
-    /**
-     * 描述文件加载成功，开始播放动画
-     * Description file loading is successful, start to play the animation
-     */
-    private startAnimation(result: string[]) {
-        let parser = new egret.HtmlTextParser();
-
-        let textflowArr = result.map(text => parser.parse(text));
-        let textfield = this.textfield;
-        let count = -1;
-        let change = () => {
-            count++;
-            if (count >= textflowArr.length) {
-                count = 0;
-            }
-            let textFlow = textflowArr[count];
-
-            // 切换描述内容
-            // Switch to described content
-            textfield.textFlow = textFlow;
-            let tw = egret.Tween.get(textfield);
-            tw.to({ "alpha": 1 }, 200);
-            tw.wait(2000);
-            tw.to({ "alpha": 0 }, 200);
-            tw.call(change, this);
-        };
-
-        change();
     }
 }
