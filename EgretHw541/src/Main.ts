70ba1d3ddba4daf49b8377e16dbd4f5af0b7e19e
ShenYang <474827518@qq.com>
2023-09-28 11:28:45 +0800

    工程就这样吧。

--- EgretHw541/src/Main.ts
+++ EgretHw541/src/Main.ts
@@ -57,6 +57,17 @@ class Main extends egret.DisplayObjectContainer {
     }
 
     /**
+     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
+     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
+     */
+        private createBitmapByName(name: string) {
+            let result = new egret.Bitmap();
+            let texture: egret.Texture = RES.getRes(name);
+            result.texture = texture;
+            return result;
+        }
+
+    /**
      * 创建游戏场景
      * Create a game scene
      */
@@ -72,8 +83,8 @@ class Main extends egret.DisplayObjectContainer {
         this.loginButton.width = 200; // 设置文本字段宽度
         this.loginButton.height = 50; // 设置文本字段高度
         this.loginButton.touchEnabled = true;
-        this.loginButton.backgroundColor = 0xFFFACD ; 
-        this.loginButton.textColor = 0xFFFFF0; 
+        // this.loginButton.backgroundColor = 0xFFFACD ; 
+        // this.loginButton.textColor = 0xFFFFF0; 
         this.loginButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onLoginButtonClick, this);
         this.addChild(this.loginButton);
         
@@ -84,8 +95,6 @@ class Main extends egret.DisplayObjectContainer {
         this.initButton = new egret.TextField();
         this.initButton.text = "初始化";
         this.initButton.touchEnabled = true;
-        // this.initButton.backgroundColor = 0xFFFFF0 // 白色
-        // this.initButton.textColor = 0x000000; // 字体颜色也设置为白色
         this.initButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onInitButtonClick, this);
         // 计算初始化按钮的位置，使其在登录按钮上方 100 像素
         this.initButton.x = (this.stage.stageWidth - this.initButton.width) / 2;
@@ -95,8 +104,6 @@ class Main extends egret.DisplayObjectContainer {
         this.payButton = new egret.TextField();
         this.payButton.text = "支付";
         this.payButton.touchEnabled = true;
-        // this.payButton.backgroundColor = 0xFFFFF0; // 白色
-        // this.payButton.textColor = 0x000000; // 字体颜色也设置为白色
         this.payButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onPayButtonClick, this);
         // 计算支付按钮的位置，使其在登录按钮下方 100 像素
         this.payButton.x = (this.stage.stageWidth - this.payButton.width) / 2;
@@ -114,48 +121,12 @@ class Main extends egret.DisplayObjectContainer {
     private onLoginButtonClick(e: egret.TouchEvent) {
         console.log("登录按钮被点击");
         // 在这里执行登录操作
-        // this.gamelogin();
-    }
 
-    // private gamelogin(){
-    //     console.log("Sign-in");
-    //     qg.gameLoginWithReal({
-    //         forceLogin:1,
-    //         appid:"103***5",
-    //         success:function(data){ 
-    //             // 登录成功后，可以存储帐号信息。             
-    //             console.log(" game login with real success:" + JSON.stringify(data));
-    //         },
-    //         fail:function(data,code){
-    //             console.log("game login with real fail:" + data + ", code:" + code);
-    //             //根据状态码处理游戏的逻辑。
-    //             //状态码为7004或者2012，表示玩家取消登录。
-    //             //此时，建议返回游戏界面，可以让玩家重新进行登录操作。
-    //             if(code ==7004||code ==2012){
-    //                 console.log("玩家取消登录，返回游戏界面让玩家重新登录。")
-    //             }
-    //             //状态码为7021表示玩家取消实名认证。
-    //             //在中国大陆的情况下，此时需要禁止玩家进入游戏。
-    //             if(code ==7021){
-    //                 console.log("The player has canceled identity verification. Forbid the player from entering the game.")
-    //             }
-    //         }
-    //     });
-    // }
+    }
     
     private onPayButtonClick(e: egret.TouchEvent) {
         console.log("支付按钮被点击");
         // 在这里执行支付操作
-    }
-
-    /**
-     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
-     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
-     */
-    private createBitmapByName(name: string) {
-        let result = new egret.Bitmap();
-        let texture: egret.Texture = RES.getRes(name);
-        result.texture = texture;
-        return result;
+        
     }
 }
